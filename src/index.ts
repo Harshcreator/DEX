f6c6608a31a36c569fcc0eb1f69d28ff08d66541
Harshcreator <jayson22603@gmai.com>
2024-07-09 08:44:14 +0530

    added functionality

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ const app = express();
 let ETH_BALANCE = 200;
 let USDC_BALANCE = 700000;
 
-// app.post('/add-liquidity', (req, res) => {});
+app.post('/add-liquidity', (req, res) => {
+    const ethAmount = req.body.ethAmount;
+    const usdcAmount = req.body.usdcAmount;
+    const product = ethAmount * usdcAmount;
+    const updatedEthQuantity = ETH_BALANCE + ethAmount;
+    const updatedUsdcBalance = product / updatedEthQuantity;
+
+    ETH_BALANCE = updatedEthQuantity;
+    USDC_BALANCE = updatedUsdcBalance
+
+    res.json({
+        message: `You added ${ethAmount} ETH and ${usdcAmount} USDC to the pool`
+    })
+});
 
 app.post('/buy-asset', (req, res) => {
     const product = ETH_BALANCE * USDC_BALANCE;
@@ -37,6 +50,15 @@ app.post('/sell-asset', (req, res) => {
     })
 });
 
-// app.post('/quote', (req, res) => {});
+app.post('/quote', (req, res) => {
+    const product = ETH_BALANCE * USDC_BALANCE;
+    const quatity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE + quatity;
+    const updatedUsdcBalance = product / updatedEthQuantity;
+
+    res.json({
+        message: `You will get ${updatedUsdcBalance} USDC for ${quatity} ETH`
+    })
+});
 
 app.listen(3000);
